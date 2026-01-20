d92ef01a52071036eca843a18dd4746b383b28c4
Christian06 <christian.quezada5968@utc.edu.ec>
2026-01-19 23:52:55 -0500

    configuracion routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClimaController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('clima.index');
 });
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/clima', [ClimaController::class, 'index'])->name('clima.index');
+    Route::post('/buscar-clima', [ClimaController::class, 'buscar'])->name('clima.buscar');
+});
+
+require __DIR__.'/auth.php';
